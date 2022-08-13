a0280c175ffb557d59e673779e5fe73857987f4d
Tederick Budiono <100257124+erickbdn@users.noreply.github.com>
2022-08-13 19:50:16 +0700

    Add files via upload

---
+++ specs/helpers/helpers/testFactories.js
@@ -0,0 +1,13 @@
+/* eslint-disable import/prefer-default-export */
+import FavoriteButtonPresenter from '../../src/scripts/utils/favorite-button-presenter';
+import FavoriteRestaurantIdb from '../../src/scripts/data/favoriterestaurant-idb';
+
+const createFavoriteButtonPresenterWithRestaurant = async (restaurant) => {
+  await FavoriteButtonPresenter.init({
+    favoriteButtonContainer: document.querySelector('#favoriteButtonContainer'),
+    favoriteRestaurants: FavoriteRestaurantIdb,
+    restaurant,
+  });
+};
+
+export { createFavoriteButtonPresenterWithRestaurant };
